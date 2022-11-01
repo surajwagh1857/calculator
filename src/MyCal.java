c0fc2b89c0c43d2ded348442d27b84597d51ce8e
surajwagh1857 <surajwagh1995@gmail.com>
2022-11-01 23:51:14 +0530

    My First commit

---
+++ src/MyCal.java
@@ -0,0 +1,15 @@
+
+public class MyCal {
+	public void add(int a,int b) {
+		System.out.println(a+b);
+	}
+	
+	public void sub(int a,int b) {
+		System.out.println(a-b);
+	}
+	
+	public void mul(int a,int b,int c) {
+		System.out.println(a*b*c);
+	}
+	
+}
---
+++ src/MyClass.java
@@ -0,0 +1,18 @@
+
+public class MyClass {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("Hi Testers.....!!");
+			System.out.println("Hi java Testers...!!#######");
+			
+			Integer num1=1000;
+			Integer num2=1000;
+			System.out.println(num1==num2);
+			Integer num3=20;
+			Integer num4=20;
+			System.out.println(num3==num4);
+		}
+	}
+
+
